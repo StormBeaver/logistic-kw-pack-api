4e546d7d11717e7f5cf17930cafa829c9b7fbfa0
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-07-01 13:40:20 +0300

    change destination folder

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -1,4 +1,4 @@
 package internal
 
-//go:generate mockgen -destination=./mocks/repo_mock.go -package=mocks github.com/stormbeaver/logistic-pack-api/internal/app/repo EventRepo
-//go:generate mockgen -destination=./mocks/sender_mock.go -package=mocks github.com/stormbeaver/logistic-pack-api/internal/app/sender EventSender
+//go:generate mockgen -destination=./internal/mocks/repo_mock.go -package=mocks github.com/stormbeaver/logistic-pack-api/internal/app/repo EventRepo
+//go:generate mockgen -destination=./internal/mocks/sender_mock.go -package=mocks github.com/stormbeaver/logistic-pack-api/internal/app/sender EventSender
