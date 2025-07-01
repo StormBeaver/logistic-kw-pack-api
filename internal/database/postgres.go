201fa0dcf0aff3fcac0d4144bbd556e256520095
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-07-01 13:45:17 +0300

    fix NewPostgres method to use new dbConn settings

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -1,26 +1,37 @@
 package database
 
 import (
+	"fmt"
+
+	sq "github.com/Masterminds/squirrel"
 	"github.com/rs/zerolog/log"
+	"github.com/stormbeaver/logistic-pack-api/internal/config"
+	"golang.org/x/net/context"
 
 	"github.com/jmoiron/sqlx"
 )
 
+// placeholder for sql requests
+var StatementBuilder = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+
 // NewPostgres returns DB
-func NewPostgres(dsn, driver string) (*sqlx.DB, error) {
+func NewPostgres(ctx context.Context, dsn, driver string, dbCons *config.DBCons) (*sqlx.DB, error) {
 	db, err := sqlx.Open(driver, dsn)
 	if err != nil {
 		log.Error().Err(err).Msgf("failed to create database connection")
-
-		return nil, err
+		return nil, fmt.Errorf("%w, sqlx.Open(%s, %s)", err, driver, dsn)
 	}
 
-	// need to uncomment for homework-4
-	// if err = db.Ping(); err != nil {
-	// 	log.Error().Err(err).Msgf("failed ping the database")
+	db.SetMaxOpenConns(dbCons.MaxOpenCons)
+	db.SetMaxIdleConns(dbCons.MaxIdleCons)
+	db.SetConnMaxLifetime(dbCons.ConnMaxLifeTime)
+	db.SetConnMaxIdleTime(dbCons.ConnMaxIdleTime)
 
-	// 	return nil, err
-	// }
+	if err = db.PingContext(ctx); err != nil {
+		log.Error().Err(err).Msgf("failed ping the database")
+
+		return nil, fmt.Errorf("%w, db.PingContext(%v)", err, ctx)
+	}
 
 	return db, nil
 }
