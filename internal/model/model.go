de1d2583ad960db7616e416322c6645ae9b49b9d
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-07-01 13:47:56 +0300

    change Pack and PackEvent structs

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,32 +1,40 @@
 package model
 
-import "fmt"
-
-type EventType uint8
-
-type EventStatus uint8
-
-const (
-	Created EventType = iota
-	Updated
-	Removed
-
-	Deferred EventStatus = iota
-	Processed
+import (
+	"encoding/json"
+	"errors"
+	"time"
 )
 
 type Pack struct {
-	ID   uint64 `db:"id"`
-	Name string `db:"Name"`
-}
-
-func (p *Pack) String() string {
-	return fmt.Sprintf("ID: %d\nName: %v\n", p.ID, p.Name)
+	ID      uint64    `db:"id"`
+	Name    string    `db:"name"`
+	Created time.Time `db:"created"`
 }
 
 type PackEvent struct {
-	ID     uint64
-	Type   EventType
-	Status EventStatus
-	Entity *Pack
+	ID     uint64 `db:"id"`
+	Type   string `db:"type"`
+	Status bool   `db:"lock"`
+	Entity *Pack  `db:"payload"`
+}
+
+// func (p *Pack) String() string {
+// 	return fmt.Sprintf("%d %s %v", p.ID, p.Name, p.Created)
+// }
+
+func (p *Pack) Scan(src any) error {
+	var pack Pack
+
+	switch src := src.(type) {
+	case []byte:
+		if err := json.Unmarshal([]byte(src), &pack); err != nil {
+			return err
+		}
+	default:
+		return errors.New("unsupported type for Scan")
+	}
+
+	*p = pack
+	return nil
 }
