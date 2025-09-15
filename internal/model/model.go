b5312eb9ce052718769936179e78c242a758fee9
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-09-15 13:24:59 +0300

    fix model

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,8 +1,6 @@
 package model
 
 import (
-	"encoding/json"
-	"errors"
 	"time"
 )
 
@@ -17,24 +15,4 @@ type PackEvent struct {
 	Type   string `db:"type"`
 	Status bool   `db:"lock"`
 	Entity *Pack  `db:"payload"`
-}
-
-// func (p *Pack) String() string {
-// 	return fmt.Sprintf("%d %s %v", p.ID, p.Name, p.Created)
-// }
-
-func (p *Pack) Scan(src any) error {
-	var pack Pack
-
-	switch src := src.(type) {
-	case []byte:
-		if err := json.Unmarshal([]byte(src), &pack); err != nil {
-			return err
-		}
-	default:
-		return errors.New("unsupported type for Scan")
-	}
-
-	*p = pack
-	return nil
 }
