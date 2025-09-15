6e12d11853bc7c23d608ef1a48f5016177cd1777
Evgeniy Orekhov <super-nutman@yandex.ru>
2025-09-15 13:25:45 +0300

    fix repo_list

--- internal/repo/repo_list.go
+++ internal/repo/repo_list.go
@@ -11,19 +11,20 @@ import (
 func (r *repo) List(ctx context.Context, cursor uint64, limit uint64) ([]*model.Pack, error) {
 	packQuery := sq.Select("id", "name", "created").
 		From("packs").
-		Where(sq.Expr("id BETWEEN ? AND ?")).
 		Where(sq.Eq{"removed": false}).
+		Offset(cursor - 1).
+		Limit(limit).
 		RunWith(r.db).
 		PlaceholderFormat(sq.Dollar)
 
-	sql, _, err := packQuery.ToSql()
+	sql, args, err := packQuery.ToSql()
 	if err != nil {
 		return nil, fmt.Errorf("convert to sql: %w", err)
 	}
 
-	packs := make([]*model.Pack, 0, limit-cursor)
+	packs := make([]*model.Pack, 0, limit)
 
-	err = r.db.SelectContext(ctx, &packs, sql, []any{cursor, limit, false}...)
+	err = r.db.SelectContext(ctx, &packs, sql, args...)
 	if err != nil {
 		return nil, fmt.Errorf("exec query: %w", err)
 	}
